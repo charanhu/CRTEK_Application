7f9dc0cb266b2da3835bad7c60d09af2573b2049
Charan H U <charanhumail@gmail.com>
2022-01-08 19:16:52 +0530

    arrays

---
+++ C++ Programs for YouTube/tut36.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int cars[4];
+cars[0]=10;
+cars[1]=20;
+cars[2]=30;
+cars[3]=40;
+for (int i = 0; i < 4; i++)
+{
+    cout<<cars[i]<<endl;
+}
+return 0;
+}
