9a0fda339eda7089c01ac0312b334acd1fb0ea27
Charan H U <charanhumail@gmail.com>
2022-01-09 18:14:25 +0530

    Array Access

--- C++ Programs for YouTube/tut37.cpp
+++ C++ Programs for YouTube/tut37.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 int main(){
 string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
-cout << cars[0];
+// cout << cars[2]<<endl;
 // Outputs Volvo
+cout<<"Before"<<endl;
+for (int i = 0; i < 4; i++)
+{
+   cout<<cars[i]<<endl;
+}
+cars[2]="Maruti Suzuki";
+cout<<"After"<<endl;
+for (int i = 0; i < 4; i++)
+{
+   cout<<cars[i]<<endl;
+}
 return 0;
 }
